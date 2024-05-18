a8c956f578baab34fa6e95fd777c3e8cec3a4270
taki <tashfiqahmed196@gmail.com>
2024-05-19 00:17:44 +0600

    23su-a

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -85,6 +85,7 @@ export default defineConfig({
                   items: [
                     { text: '23 Fall - A',   link: '/spl/lab/solve-fin/23fa-A' },
                     { text: '23 Fall - B',   link: '/spl/lab/solve-fin/23fa-B' },
+                    { text: '23 Summer - A', link: '/spl/lab/solve-fin/23su-A' },
                     { text: '23 Spring - A', link: '/spl/lab/solve-fin/23sp-A' },
                     { text: '23 Spring - B', link: '/spl/lab/solve-fin/23sp-B' },
                     { text: '22 Fall',       link: '/spl/lab/solve-fin/22fa' },
